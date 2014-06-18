41a90abf670f0cdc1daa71f2989cd22a13e76680
lcbunch <lbunch@vt.edu>
2014-06-18 14:13:16 -0600

    changes to postgresql

--- blogapp/db/seeds.rb
+++ blogapp/db/seeds.rb
@@ -1,7 +1,3 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+Post.create(title: 'My first post', author: 'Lisa Bunch', description: 'This is a super cool blog post. Welcome to Rails.')
+Post.create(title: 'My second post', author: 'Lisa Bunch', description: 'This is a super cool blog post. Welcome to Rails.')
+Post.create(title: 'My third post', author: 'Lisa Bunch', description: 'This is a super cool blog post. Welcome to Rails.')
